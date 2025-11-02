f51ea4e613ae5c2f2079f6204d083c34834dd209
Javier0618 <99229354+Javier0618@users.noreply.github.com>
2025-11-01 21:13:38 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNeedBwkLS_U0a048mZ233DSM-bMh789U",
+  authDomain: "streamfusion-app.firebaseapp.com",
+  projectId: "streamfusion-app",
+  storageBucket: "streamfusion-app.firebasestorage.app",
+  messagingSenderId: "1023470476009",
+  appId: "1:1023470476009:web:f398cd8daf7cf55b782e78",
+  measurementId: "G-35C7CJHLKB"
+};
+
+const API_KEY = '32e5e53999e380a0291d66fb304153fe';
+
+export { firebaseConfig, API_KEY };
